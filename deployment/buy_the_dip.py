45b97035e9db0f055b9c89886275044269ef33e4
ryan.bacastow <ryan.bacastow@tmsw.com>
2020-04-21 01:50:06 -0500

    working

--- deployment/buy_the_dip.py
+++ deployment/buy_the_dip.py
@@ -1,33 +1,28 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import yfinance as yf
-import boto3
-from os import environ as env
-
 
 def calc_stock(high, current):
-    return (current - high)/high
+    ratio = (current - high) / high
+    return ratio
 
 
-def convert_tuple(tup): 
-    string_tup =  "{} : {}".format(tup[0], str(tup[1]))
+def convert_tuple(tup):
+    string_tup = "{} : {}".format(tup[0], str(tup[1]))
     return string_tup
 
 
 def create_message(pairs):
-    message = "Ordered Portfolio:\n"
+    message = "\nORDERED RATIOS:\n\n"
     for pair in pairs:
         message += convert_tuple(pair) + "\n"
     return message
-    
-    
+
+
 def publish_message_sns(message):
     sns_arn = env.get('SNS_ARN').strip()
     sns_client = boto3.client('sns')
     try:
-        print(message)
-
         response = sns_client.publish(
             TopicArn=sns_arn,
             Message=message
@@ -39,39 +34,46 @@ def publish_message_sns(message):
         print("ERROR PUBLISHING MESSAGE TO SNS: {}".format(e))
 
 
-def read_tickers(file='stock_tickers.txt', period='5y'):
-      
+def read_tickers(file='deployment/stock_tickers.txt', period='5y'):
     pairs = dict()
+    out_string = "\nINDIVIDUAL STATS:\n"
 
     with open(file, 'r') as f:
         while True:
+            temp_string = ""
             try:
                 # Get next line from file
                 ticker = (f.readline()).strip()
 
                 if ticker == "":
                     break
-                else:
-                    print(ticker)
 
                 stock = yf.Ticker(ticker)
                 data = stock.history(env.get('PERIOD', period))
 
                 close = data.Close[-1]
+                temp_string += "{} Close: {}\n".format(ticker, str(close))
+
                 high = max(data.Close)
+                temp_string += "{} {}-High: {}\n".format(ticker, env.get('PERIOD', period), str(highh))
 
                 delta = calc_stock(high, close)
+
+                temp_string += "{} Delta: {}\n".format(ticker, str(delta))
+
+                out_string += "\n" + temp_string
 
                 pairs[ticker] = delta
 
                 if not ticker:
                     break
+
             except Exception as e:
                 print(e)
                 if not ticker:
                     break
-        
-    return(sorted(pairs.items(), key=lambda x: x[1]))
+
+    return out_string, sorted(pairs.items(), key=lambda x: x[1])
 
 
 def handler(event, context):
@@ -79,9 +81,10 @@ def handler(event, context):
     This function drives the AWS lambda. Requires 1 env var to work correctly: SNS_TOPIC which represents the topic arn to which
     you want to publish. 
     """
-    pairs = read_tickers()
+
+    out_string, pairs = read_tickers()
     message = create_message(pairs)
+    message += out_string
     print(message)
-    #AWS LIVE VERSION ONLY:
     publish_message_sns(message)
 
